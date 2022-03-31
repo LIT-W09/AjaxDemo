27af7669a0537404c719e870e38fe5fd42f3f7af
Avrumi Friedman <a.friedman07@gmail.com>
2022-03-30 21:41:19 -0400

    Add project files.

---
+++ AjaxDemo/Models/PeopleDb.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxDemo.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class PeopleDb
+    {
+        private string _connectionString;
+
+        public PeopleDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Person> GetAll()
+        {
+            using var conn = new SqlConnection(_connectionString);
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM People";
+            conn.Open();
+            var reader = cmd.ExecuteReader();
+            List<Person> ppl = new();
+            while (reader.Read())
+            {
+                ppl.Add(new Person
+                {
+                    Id = (int)reader["Id"],
+                    FirstName = (string)reader["FirstName"],
+                    LastName = (string)reader["LastName"],
+                    Age = (int)reader["Age"],
+
+                });
+            }
+
+            return ppl;
+        }
+
+        public void AddPerson(Person person)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "INSERT INTO People (FirstName, LastName, Age) " +
+                "VALUES(@f, @l, @a) SELECT SCOPE_IDENTITY()";
+            cmd.Parameters.AddWithValue("@f", person.FirstName);
+            cmd.Parameters.AddWithValue("@l", person.LastName);
+            cmd.Parameters.AddWithValue("@a", person.Age);
+            conn.Open();
+            person.Id = (int)(decimal)cmd.ExecuteScalar();
+        }
+    }
+}
---
+++ AjaxDemo/wwwroot/js/people.js
@@ -0,0 +1,34 @@
+﻿$(() => {
+
+    loadPeople();
+
+    function loadPeople() {
+        $.get('/people/getall', function (people) {
+            $("#people-table tr:gt(0)").remove();
+            people.forEach(person => {
+                $("#people-table tbody").append(`
+<tr>
+    <td>${person.firstName}</td>
+    <td>${person.lastName}</td>
+    <td>${person.age}</td>
+
+</tr>`);
+            });
+        });
+    }
+
+    $("#add-person").on('click', function () {
+        const firstName = $("#first-name").val();
+        const lastName = $("#last-name").val();
+        const age = $("#age").val();
+
+
+        $.post('/people/addperson', { firstName, lastName, age }, function (person) {
+            //console.log(person.id);
+            loadPeople();
+            $("#first-name").val('');
+            $("#last-name").val('');
+            $("#age").val('');
+        });
+    });
+})
